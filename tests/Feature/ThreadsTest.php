58f839ebd8bd908d9608f17887f29a4bdd7058b8
Dylan Wilkinson <dwilkinson167@southseattle.edu>
2017-12-24 11:44:58 -0800

    More Tests

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -5,15 +5,24 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ThreadsTest extends TestCase
-{
+class ThreadsTest extends TestCase {
 	use DatabaseMigrations;
-    /** @test */
-    public function a_user_can_browse_threads()
-    {
-    	$thread = factory('App\Thread')->create();
-        $response = $this->get('/threads');
 
-        $response->assertSee($thread->title);
-    }
+	/** @test */
+	public function a_user_can_view_all_threads() {
+		$thread = factory( 'App\Thread' )->create();
+
+		$response = $this->get( '/threads' );
+		$response->assertSee( $thread->title );
+
+
+	}
+
+	/** @test */
+	function a_user_can_read_a_single_thread() {
+		$thread = factory( 'App\Thread' )->create();
+
+		$response = $this->get( '/threads/' . $thread->id );
+		$response->assertSee( $thread->title );
+	}
 }
